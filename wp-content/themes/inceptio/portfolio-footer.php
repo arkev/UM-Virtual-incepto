7ef4f5e7f1ff95e20cf8c5f0f54f4b3b97c7d95c
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2015-02-23 11:39:26 -0600

    Educación continua
    Se cargó toda la información de la educación continua y se hicieron
    correcciones en misión, visión y acerca de

--- wp-content/themes/inceptio/portfolio-footer.php
+++ wp-content/themes/inceptio/portfolio-footer.php
@@ -4,7 +4,7 @@
         array('count' => 8,
             'related' => 'true',
             'display_meta' => 'true',
-            'tca' => 'lightbox'));
+            'tca' => 'link'));
     $related_posts = do_shortcode('[post_gallery count="'.$related_posts_config['count'].'" related="'.$related_posts_config['related'].'" display_mode="carousel" tca="'.$related_posts_config['tca'].'" display_meta="'.$related_posts_config['display_meta'].'" ][/post_gallery]'); ?>
 <?php if(!empty($related_posts)){ ?>
 <section>
