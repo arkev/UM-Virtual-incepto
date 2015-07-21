2e1568a94db74716a39724b6fab5d13b47131596
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2015-07-21 12:28:46 -0500

    correcciones a los formularios de las maestrias

--- maestrias/finanzas/send.php
+++ maestrias/finanzas/send.php
@@ -1,11 +1,38 @@
-<?		
-$MailTo="umvirtual@um.edu.mx";
+<?php
+//Datos
 $nombre=$_POST['nombre'];
 $email=$_POST['email'];
-$contenido='
-Nombre: '.utf8_decode($nombre).' 
-E-mail: '.utf8_decode($email).'
-';
-mail($MailTo, "$Subject $nombre está solicitando una beca en la Maestría en administración con acentuación en finanzas", $contenido, "From: $email");
+$to = "allenzapien@um.edu.mx";
+$subject = "$nombre está solicitando una beca en la Maestría en Finansas";
+
+//Mensaje
+$message = "
+<html>
+<head>
+<title>$subject</title>
+</head>
+<body>
+<table>
+<tr>
+<th>Nombre:/th>
+<th>E-mail:</th>
+</tr>
+<tr>
+<td>$nombre</td>
+<td>$email</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+//Headers
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: $email' . "\r\n";
+
+//Funciones
+mail($to,$subject,$message,$headers);
 header('Location: gracias.html');
+exit;
 ?>
--- maestrias/test/send.php
+++ maestrias/test/send.php
@@ -1,9 +1,37 @@
-<?		
-$MailTo="allenzapien@um.edu.mx";
+<?php
+//Datos
 $email=$_POST['email'];
-$contenido='
-E-mail: '.utf8_decode($email).'
-';
-mail($MailTo, "$Subject interesado en el test de maestrías", $contenido, "From: $email");
-header('Location: gracias.html');
+$to = "allenzapien@um.edu.mx";
+$from = "$email>";
+$subject = "correo de una persona que quiere el test de maestrias";
+
+//Mensaje
+$message = "
+<html>
+<head>
+<title>$subject</title>
+</head>
+<body>
+<table>
+<tr>
+<th>E-mail:</th>
+</tr>
+<tr>
+<td>$email</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+//Headers
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type:text/html;charset=UTF-8";
+$headers[] = "From: $from";
+
+
+//Funciones
+mail($to,$subject,$message,implode("\r\n", $headers));
+echo"<script>window.location='gracias.html'</script>;"
 ?>
