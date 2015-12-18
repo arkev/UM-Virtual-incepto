7c4f48d8fbd6c81487bf9dd0a200153ad51718db
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2015-12-18 09:58:00 -0600

    Las ocho claves de seguridad

--- wp-config.php
+++ wp-config.php
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_KEY', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('SECURE_AUTH_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('LOGGED_IN_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
-define('NONCE_SALT', 'pon aquí tu frase aleatoria'); // Cambia esto por tu frase aleatoria.
+define('AUTH_KEY',         '+7(oP8sE*^o}ckkOS+-mI0-J|?{oa@9c.iTC[~H.YD{Kvd:-[65F5AopEim-O z>');
+define('SECURE_AUTH_KEY',  'lriFUBVn-gL[?*o6Zc>W1pA:bQT5>hjjQW*G78zZZ!zQp%nJ;O7nDD-AMu%R<Rb(');
+define('LOGGED_IN_KEY',    'V},XB&~&wQqYXWKcC8fGaQD;-kofn/tYeIoA@qTHXszD~x$hgL&m>gWQH-O501S5');
+define('NONCE_KEY',        'DZ><NMa4lyvGrW|f,+s.i-d|Hddu+1WK;HRwJ@Im}sVJl}=HRy9DLc|JM&1V5Zo%');
+define('AUTH_SALT',        '9F9z_!6>;~|)cev}xs[?[x2sRi?B}Z6ZN[`}ReX(<U@KeSIg-S;9&87&>Pn}DC8G');
+define('SECURE_AUTH_SALT', '|0A8H{g?p$SV=-+~+s`dm_ZIfgQn8I7-+at#.MVJe3EBp-g3mbH]ImKKWhlER8]4');
+define('LOGGED_IN_SALT',   'L8+F_%^T7Uk-4aONr#YP5J2,5bUu+F4wXH45:F~qpE!$dTc;O#fdfcCvxNv}V&mb');
+define('NONCE_SALT',       'e$abOjT|)Z.7Br3E7jWB/ji(}<YK<NfeOPmk0EEnecv=b9CEDr{zrNmXW^ ,?@[(');
 
 /**#@-*/
 
