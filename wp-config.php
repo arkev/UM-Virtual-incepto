e1f22764c8a3b5e1b966428602bde57cefda8a43
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2016-07-05 15:38:12 -0500

    últimos cambios a esta web

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** Ajustes de MySQL. Solicita estos datos a tu proveedor de alojamiento web. ** //
 /** El nombre de tu base de datos de WordPress */
-define('DB_NAME', 'umv');
+define('DB_NAME', 'umvirtual_actual');
 
 /** Tu nombre de usuario de MySQL */
-define('DB_USER', 'umv');
+define('DB_USER', 'root');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'sitioNuevoUMVirt.15');
+define('DB_PASSWORD', 'root');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
